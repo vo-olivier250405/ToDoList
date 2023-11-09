eac4385d0ab3755f029c6b1636b0f999baedb397
vo_o <vo_o@etna-alternance.net>
2023-11-09 11:58:12 +0100

    [ADD] => index.ts => Ajout d'une fonction qui supprime une tâche

--- scripts/index.js
+++ scripts/index.js
@@ -9,16 +9,29 @@ function addTask() {
     else {
         li.innerHTML = inputBox === null || inputBox === void 0 ? void 0 : inputBox.value;
         todoList === null || todoList === void 0 ? void 0 : todoList.appendChild(li);
+        li.setAttribute("onclick", "changeClass(this)");
+        // li.onclick = changeClass(HTMLLinkElement)
     }
     inputBox.value = "";
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
+    span.setAttribute("onclick", "deleteElement(this)");
     li.appendChild(span);
 }
-function changeClass(event) {
-    console.log(event);
+function changeClass(element) {
+    console.log(element.classList);
     //   const tasks = document.querySelector(`#task-${id}`) as HTMLDListElement;
     //   console.log(tasks);
-    event.classList.add("checked");
+    if (element.className === "") {
+        element.className = "checked";
+    }
+    else {
+        element.className = "";
+    }
     //   const truc = (document.getElementById("tasks")?.className = "checked");
+}
+function deleteElement(element) {
+    var _a;
+    console.log(element.parentElement);
+    (_a = element.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
 }
--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,31 @@ function addTask(): void {
   } else {
     li.innerHTML = inputBox?.value;
     todoList?.appendChild(li);
+    li.setAttribute("onclick", "changeClass(this)");
+    // li.onclick = changeClass(HTMLLinkElement)
   }
   inputBox.value = "";
   let span = document.createElement("span");
   span.innerHTML = "\u00d7";
+  span.setAttribute("onclick", "deleteElement(this)");
   li.appendChild(span);
 }
 
-function changeClass(elemnt: HTMLLinkElement) {
-    console.log(elemnt);
-    
-//   const tasks = document.querySelector(`#task-${id}`) as HTMLDListElement;
-//   console.log(tasks);
-  elemnt.classList.add("checked");
+function changeClass(element: HTMLLinkElement) {
+  console.log(element.classList);
+
+  //   const tasks = document.querySelector(`#task-${id}`) as HTMLDListElement;
+  //   console.log(tasks);
+  if (element.className === "") {
+    element.className = "checked";
+  } else {
+    element.className = "";
+  }
 
   //   const truc = (document.getElementById("tasks")?.className = "checked");
+}
+
+function deleteElement(element: HTMLSpanElement) {
+  console.log(element.parentElement);
+  element.parentElement?.remove();
 }
